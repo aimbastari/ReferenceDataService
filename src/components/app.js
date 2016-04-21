93880883038adab0f8b4dfc3ae090248f3d6c749
aimbastari <aimbastari@gmail.com>
2016-04-21 10:57:38 -0700

    remove redux dependencies

--- src/components/app.js
+++ src/components/app.js
@@ -1,33 +1,66 @@
 import React from 'react';
 import { Component } from 'react';
-import SearchBar from '../containers/search_bar';
-import SearchDetail from '../containers/search_detail';
+import axios from 'axios';
+
+import SearchBar from './search_bar';
 import CusipList from './cusip_list';
+import SwapDetail from './swap_detail';
+
+
+const SWAP_API = 'http://localhost/SME/';
 
 
 export default class App extends Component {
 
   constructor(props){
     super(props);
-    this.state = {cusips: ['xyz1232', 'xyz12366', 'x1231233']};
+    this.state = {cusips: ['xyz1232xs', 'xyz12366', 'x1231233']};
 
   }
 
   //Call the SME backend to get list of cusips
-  //Use the angio library
   cusipSearch(term){
+    //Build request url
+    const url = `${SWAP_CUSIP_API}cusip/${term}`;
 
-
+    //make the request
+    const request = axios.get(url)
+      .then((response) => {
+        //setting state will trigger all components to rerender themselves
+        this.setState({cusips: response});
+      })
+      .catch((response) => {
+          console.log("Error fetching cusips!");
+      });
 
   }
+
+  //Call the SME backend to get list of cusips
+  swapSearch(cusip){
+    //Build request url
+    const url = `${SWAP_CUSIP_API}/security/${cusip}`;
+
+    //make the request
+    const request = axios.get(url)
+      .then((response) => {
+        //setting state will trigger all components to rerender themselves
+        this.setState({swap: response});
+      })
+      .catch((response) => {
+          console.log("Error fetching  Security!");
+      });
+
+  }
+
+
 
   render() {
     return (
       <div>
         <div className='sme-header'>Swap Maintenance</div>
         <div><SearchBar onSearchTermChange={term => this.cusipSearch(term)} /></div>
-        <div className='sme-search-detail'><CusipList cusips={this.state.cusips}/></div>
-        <div>Swap Detail </div>
+        <div><CusipList cusips={this.state.cusips} onCusip/></div>
+        <div className='sme-search-detail' onSwapChange><SwapDetail /></div>
       </div>
     );
   }
---
+++ src/components/swap_detail.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class SwapDetail extends Component{
+  constructor(props) {
+    super(props);
+    this.state = { swap : {}};
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event){
+    this.setState({ term: event.target.value});
+
+  }
+
+  onFormSubmit(event){
+    event.preventDefault();
+
+    //fetch cusips from backend here
+    this.props.onSwapChange(this.state.swap);
+
+  }
+
+  render(){
+    return (
+      <div>Swap Detail</div>
+    );
+  }
+}
+
+export default SwapDetail;
